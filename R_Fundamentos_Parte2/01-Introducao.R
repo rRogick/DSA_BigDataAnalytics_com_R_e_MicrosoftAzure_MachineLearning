d9583fb04230745b687e34196c2ae9d53a902e44
Renan Rogick <rRogick@users.noreply.github.com>
2018-12-24 14:16:18 -0200

    Create 01-Introducao.R

---
+++ R_Fundamentos_Parte2/01-Introducao.R
@@ -0,0 +1,23 @@
+# Revisao e Aprofundamento
+
+# Interagindo com o sistema operacional
+list.files()
+.Platform$OS.type
+.Platform$file.sep
+Sys.Date()
+Sys.info()
+R.version
+?system
+system('ls -a')
+
+
+
+# Criando um Vetor
+vec1 <- c(12, 3, 4, 19, 34)
+vec1
+
+
+# Criando uma Matriz
+dados <- c(1:50)
+mat1 <- matrix(dados, nrow = 5, ncol = 5, byrow = T)
+mat1
