436f07f7c81388299be5c876cc9b93fb1ddfb927
Renan Rogick <rRogick@users.noreply.github.com>
2018-12-30 16:21:27 -0200

    Create 06-Funcoes Especiais.R

---
+++ R_Fundamentos_Parte2/06-Funcoes Especiais.R
@@ -0,0 +1,143 @@
+# Funcoes Especiais
+
+# unlist()
+# Produz um vetor com os elementos da lista
+?unlist
+
+lst1 <- list(6,"b",15)
+lst1
+class(lst1)
+
+unlist(lst1)
+vec1 <- unlist(lst1)
+class(vec1)
+vec1
+
+lst2 <- list(v1 = 6, v2 = list(381, 2190), v3 = c(30, 217))
+lst2
+
+unlist(lst2)
+
+mean(unlist(lst2))
+round(mean(unlist(lst2)))
+
+
+# do.call()
+# Executa uma funcao em um objeto
+# *** Atencao ***
+# As funcoes da familia apply aplicam uma funcao a cada elemento de um objeto (substitui um loop)
+# A funcao do.call aplica uma funcao ao objeto inteiro e nao a cada elemento individualmente
+?do.call
+
+data <- list()
+N <- 100
+
+for (n in 1:N) {
+  data[[n]] = data.frame(index = n, char = sample(letters, 1), z = rnorm(1))
+}
+
+head(data)
+
+do.call(rbind, data)
+class(do.call(rbind, data))
+
+# lapplly() x do.call()
+y <- list(1:3, 4:6, 7:9)
+y
+
+lapply(y, sum) # linha a linha
+do.call(sum, y) # todo objeto
+
+
+# O resultado da funcao do.call() pode ser obtido de outra forma
+# Pacote plyr
+
+install.packages('plyr')
+library(plyr)
+
+y <- list(1:3, 4:6, 7:9)
+y
+
+ldply(y, sum)
+
+
+# Benchmark
+install.packages('rbenchmark')
+library(rbenchmark)
+benchmark(do.call(sum, y), ldply(y, sum))
+
+N <- list(as.numeric(1:30000), as.numeric(4:60000), as.numeric(7:90000))
+
+benchmark(do.call(sum, N), ldply(N, sum))
+
+
+# strplit()
+# Divide uma string ou vetor de caracteres
+
+texto <- "Esta e uma string."
+strsplit(texto, " ")
+
+texto <- "Esta e uma string."
+strsplit(texto, "")
+
+
+dates <- c("1999-05-23", "2001-12-30", "2004-12-17")
+temp <- strsplit(dates, "-")
+temp
+class(temp)
+
+matrix(unlist(temp), ncol = 3, byrow = TRUE)
+
+Names <- c("Brian, Sergey", "Page, Larry",
+           "Dorsey, Jack", "Glass, Noah",
+           "Williams, Evan", "Stone, Biz"
+           )
+
+Cofounded <- rep(c("Google", "Twitter"), c(2,4))
+temp <- strsplit(Names, ", ")
+temp
+
+
+frase <- "Muitas vezes temos que repetir algo diversas vezes e essas diversas vezes parecem algo estranho"
+palavras <- strsplit(frase, " ")[[1]]
+palavras
+unique(tolower(palavras))
+
+
+antes = data.frame(attr = c(1,30,4,6), tipo = c('pao_e_agua','pao_e_agua_2'))
+antes
+strsplit(as.character(antes$tipo), '_e_') # retorna lista
+
+
+library(stringr)
+str_split_fixed(antes$tipo, "_e_", 2) # retorna matriz
+
+
+antes = data.frame(attr = c(1,30,4,6), tipo = c('pao_e_agua','pao_e_agua_2'))
+antes
+depois <- strsplit(as.character(antes$tipo),'_e_')
+do.call(rbind, depois) # transforma em matriz
+
+
+
+
+library(dplyr)
+install.packages("tidyr")
+library(tidyr)
+
+antes <- data.frame(
+  attr = c(1, 30 ,4 ,6 ), 
+  tipo = c('pao_e_agua','pao_e_agua_2')
+)
+
+antes %>%
+  separate(tipo, c("pao", "agua"), "_e_")
+
+
+
+
+# Para encerrar
+
+# Operadores de atribuicao
+
+
